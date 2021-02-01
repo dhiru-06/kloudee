a91c41a652c19e4e85cf2699bbd73bfed38e60b9
dhiru-06 <dhiru3764@gmail.com>
2021-02-01 16:43:48 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import giphy from './assests/giphy2.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={giphy} className="App-logo" alt="logo" />
+        <h1>
+         Under Construction
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Our Website is currently undergoing scheduled maintenance.
+          We should be back shortly. Thank you for your patience
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input className="input" type="email" placeholder="Enter your email" id="email" name="email" ></input>
+        <input type="submit" className="btn" value="Submit"></input>
+        <p className="bottom">
+          Sign up now to get early notification of our launch date!
+        </p>
       </header>
     </div>
   );
